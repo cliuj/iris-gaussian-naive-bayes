b4a32f0e3096dd74c641643040684f537ff57ec9
jliu2179 <jliu2179@gmail.com>
2018-11-29 21:06:15 -0500

    modified the way to obtain the training set, added a mean function for the flower features

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ if (not has_dataset):
 
 class Flower:
     def __init__(self, sepal_length, sepal_width, petal_length, petal_width, flower_class):
-        self.sepal_length = sepal_length
-        self.sepal_width = sepal_width
-        self.petal_length = petal_length
-        self.petal_width = petal_width
+        self.sepal_length = float(sepal_length)
+        self.sepal_width  = float(sepal_width)
+        self.petal_length = float(petal_length)
+        self.petal_width  = float(petal_width)
         self.flower_class = flower_class
 
     def to_string(self):
@@ -85,29 +85,43 @@ def retrieve_data_from(dataset_file):
         flowers_dataset.append(Flower(sepal_length, sepal_width, petal_length, petal_width, flower_class))
 
 
-def draw_samples_for_training(percentage_to_train):
-    if percentage_to_train > 0 and percentage_to_train <= len(flowers_dataset):
-        for i in range(0, int(percentage_to_train * len(flowers_dataset))):
-            training_set.append(flowers_dataset[i])
+def draw_samples_for_training():
+    for setosa in range(0, 50):
+        if setosa == 25:
+            break
+        training_set.append(flowers_dataset[setosa])
+
+    for versicolor in range(50, 100):
+        if versicolor == 75:
+            break
+        training_set.append(flowers_dataset[versicolor])
+
+    for virginica in range(100, len(flowers_dataset)):
+        if virginica == 125:
+            break
+        training_set.append(flowers_dataset[virginica])
 
 
+
+# get the data from dataset.txt and store the data into
+# a list of flowers
 flowers_dataset = []
 retrieve_data_from(dataset_file)
 
+#if debug:
+#    print("Unshuffled flowers dataset:")
+#    print_contents_of(flowers_dataset)
+#
+#shuffle(flowers_dataset)
+#if debug:
+#    print("Shuffled flowers dataset:")
+#    print_contents_of(flowers_dataset)
 
-if debug:
-    print("Unshuffled flowers dataset:")
-    print_contents_of(flowers_dataset)
 
-shuffle(flowers_dataset)
-if debug:
-    print("Shuffled flowers dataset:")
-    print_contents_of(flowers_dataset)
-
-
+# setup the training set
 training_set = []
 
-draw_samples_for_training(2/3)
+draw_samples_for_training()
 
 if debug:
     print("Training set:")
@@ -118,12 +132,76 @@ if debug:
     print("Size of training_set: {}".format(len(training_set)))
 
 
+
 # perfom naive bayes training
 
+# get the mean of the flower's features
+def sepal_length_mean(flower_class, start, end):
+    sigma = 0
+    mean = 0
+    for flower in range(start, end):
+        sigma = sigma + training_set[flower].sepal_length
+    mean = sigma / (end - start)
+    flower_class['mean'].append(mean)
+
+def sepal_width_mean(flower_class, start, end):
+    sigma = 0
+    mean = 0
+    for flower in range(start, end):
+        sigma = sigma + training_set[flower].sepal_width
+    mean = sigma / (end - start)
+    flower_class['mean'].append(mean)
+
+def petal_length_mean(flower_class, start, end):
+    sigma = 0
+    mean = 0
+    for flower in range(start, end):
+        sigma = sigma + training_set[flower].petal_length
+    mean = sigma / (end - start)
+    flower_class['mean'].append(mean)
+
+def petal_width_mean(flower_class, start, end):
+    sigma = 0
+    mean = 0
+    for flower in range(start, end):
+        sigma = sigma + training_set[flower].petal_width
+    mean = sigma / (end - start)
+    flower_class['mean'].append(mean)
 
 
 
+setosa = {
+    'mean' : [],
+    'std' : []
+}
+
+versicolor = {
+    'mean' : [],
+    'std' : []
+}
+
+virginica = {
+    'mean' : [],
+    'std' : [],
+}
 
 
+sepal_length_mean(setosa, 0, 25)
+sepal_width_mean(setosa,  0, 25)
+petal_length_mean(setosa, 0, 25)
+petal_width_mean(setosa,  0, 25)
+
+sepal_length_mean(versicolor,25, 50)
+sepal_width_mean(versicolor, 25, 50)
+petal_length_mean(versicolor,25, 50)
+petal_width_mean(versicolor, 25, 50)
+
+sepal_length_mean(virginica, 50, 75)
+sepal_width_mean(virginica,  50, 75)
+petal_length_mean(virginica, 50, 75)
+petal_width_mean(virginica,  50, 75)
 
 
+print("setosa: \t{}".format(setosa['mean']))
+print("versicolor: \t{}".format(versicolor['mean']))
+print("virginica: \t{}".format(virginica['mean']))
